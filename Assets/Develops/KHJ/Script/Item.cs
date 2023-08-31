2735b3aadaad7914bc8bde362094c33690c84060
권혁진 <lazy82531@gamil.com>
2023-08-31 21:48:54 +0900

    fix : Item 수정

--- Assets/Develops/KHJ/Script/Item.cs
+++ Assets/Develops/KHJ/Script/Item.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public abstract class Item
 {
+    public ItemData Data { get; private set; }
 
+    public Item(ItemData data) => Data = data;
 }
